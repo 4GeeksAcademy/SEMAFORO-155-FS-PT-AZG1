1bb16b0a891aab9d9a9ae9a4398465b5b8a559d2
albertoZG <albertozg94@gmail.com>
2025-07-12 20:53:49 +0000

    semaforo done

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,28 +1,64 @@
-import React from "react";
+import React, { useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-            
+  const [color, setColor] = useState("yellow");
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+  // const colores = ["red", "yellow", "green"];
+
+  // const cambiarColor = () => {
+  //   const actualIndex = colores.indexOf(color);
+  //   const nextIndex = (actualIndex + 1) % colores.length;
+  //   setColor(colores[nextIndex]);
+  // }
+
+  return (
+    <div className="d-flex flex-column align-items-center mt-5">
+      <div
+        style={{
+          width: "13px",
+          height: "100px",
+          backgroundColor: "black",
+        }}
+      ></div>
+
+      
+      <div
+        className="d-flex flex-column align-items-center bg-dark p-4 rounded"
+        style={{ width: "125px" }}
+      >
+        <div
+          className={`rounded-circle bg-danger m-2 p-4 ${
+            color === "red" ? "opacity-100" : "opacity-25"
+          }`}
+		  style={{width: "80px", height: "80px"}}
+          onClick={() => setColor("red")}
+        ></div>
+
+        <div
+          className={`rounded-circle bg-warning m-2 p-4 ${
+            color === "yellow" ? "opacity-100" : "opacity-25"
+          }`}
+		  style={{width: "80px", height: "80px"}}
+          onClick={() => setColor("yellow")}
+        ></div>
+
+        <div
+          className={`rounded-circle bg-success m-2 p-4 ${
+            color === "green" ? "opacity-100" : "opacity-25"
+          }`}
+		  style={{width: "80px", height: "80px"}}
+          onClick={() => setColor("green")}
+        ></div>
+      </div>
+{/* <button className="btn btn-primary mt-3" onClick={cambiarColor}>
+  Cambiar color!
+</button> */}
+
+    </div>
+  );
 };
 
 export default Home;
+
+
+
--- src/js/main.jsx
+++ src/js/main.jsx
@@ -11,7 +11,7 @@ import '../styles/index.css'
 // components
 import Home from './components/Home';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const root=ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Home/>
   </React.StrictMode>,
